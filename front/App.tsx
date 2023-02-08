cc50b708f951373bdfc37f728f718920546a2e30
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-08 15:06:13 +0100

    dev: added css and global css

--- front/App.tsx
+++ front/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {gs} from './styles';
 
-function App(): JSX.Element {
+function App() {
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text style={[gs.text, styles.text]}>Hello world</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 50,
+  },
+});
 
 export default App;
---
+++ front/styles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const gs = StyleSheet.create({
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 20,
+  },
+});
