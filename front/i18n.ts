9dd4fe126329e14c72f217a9e0145eb32c63a44d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-08 16:57:32 +0100

    dev: added a little bit of i18n

---
+++ front/i18n.ts
@@ -0,0 +1,4 @@
+import tFr from './i18n/fr.json';
+import tEn from './i18n/en.json';
+
+export const t = tEn;
--- front/screens/LoginScreen.tsx
+++ front/screens/LoginScreen.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {t} from '../i18n';
 import {gs} from '../styles';
 
 function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Text style={[gs.text, gs.title]}>Connexion</Text>
+      <Text style={[gs.text, gs.title]}>{t.loginScreen.title}</Text>
       <View style={gs.form}>
         <View style={gs.label}>
-          <Text style={gs.text}>Identifiant</Text>
+          <Text style={gs.text}>{t.login}</Text>
           <TextInput style={gs.input} />
         </View>
         <View style={gs.label}>
-          <Text style={gs.text}>Mot de passe</Text>
+          <Text style={gs.text}>{t.password}</Text>
           <TextInput style={gs.input} />
         </View>
         <View style={gs.submitButton}>
