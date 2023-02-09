1da6fa48c799cc1234ea4cec47788c4053edca9f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-09 14:26:26 +0100

    dev: prefer not using let / else

--- front/screens/MenuScreen.tsx
+++ front/screens/MenuScreen.tsx
@@ -10,26 +10,35 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator<RootStackParamList>();
 
+const getIconName = (name: string, focused: boolean) => {
+  if (name === 'Home') {
+    return focused
+      ? 'ios-information-circle'
+      : 'ios-information-circle-outline';
+  }
+  if (name === 'Settings') {
+    return focused ? 'ios-list' : 'ios-list-outline';
+  }
+  return 'ios-list';
+};
+
 function MenuScreen() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Home"
         screenOptions={({route}) => ({
+          // We have disable the linter because this is the example from the react navigation doc.
           // eslint-disable-next-line react/no-unstable-nested-components
           tabBarIcon: ({focused, color, size}) => {
-            let iconName = 'ios-list';
-
-            if (route.name === 'Home') {
-              iconName = focused
-                ? 'ios-information-circle'
-                : 'ios-information-circle-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'ios-list' : 'ios-list-outline';
-            }
-
             // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
+            return (
+              <Ionicons
+                name={getIconName(route.name, focused)}
+                size={size}
+                color={color}
+              />
+            );
           },
           tabBarActiveTintColor: 'tomato',
           tabBarInactiveTintColor: 'gray',
