bee84cac6876f47dea1717f8b03781b45e224989
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-09 09:39:52 +0100

    dev: state component
    useState + useEffect

--- front/App.tsx
+++ front/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import LoginScreen from './screens/LoginScreen';
 import MenuScreen from './screens/MenuScreen';
+import SplashScreen from './screens/SplashScreen';
 
 function App() {
-  return <MenuScreen />;
+  const [showSplashScreen, setShowSplashScreen] = useState(true);
+  const isConnected = false;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplashScreen(false);
+      console.log('change');
+    }, 2000);
+  }, []);
+
+  return showSplashScreen ? (
+    <SplashScreen />
+  ) : isConnected ? (
+    <MenuScreen />
+  ) : (
+    <LoginScreen />
+  );
 }
 
 export default App;
