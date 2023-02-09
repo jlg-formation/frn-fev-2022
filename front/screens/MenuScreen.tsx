6951b43c1ca09f1d8883969e3e6182562ad28b4c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-09 12:13:17 +0100

    dev: added react navigation

--- front/screens/MenuScreen.tsx
+++ front/screens/MenuScreen.tsx
@@ -1,16 +1,18 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {HomeScreen} from './HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Menu</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
 
 export default MenuScreen;
