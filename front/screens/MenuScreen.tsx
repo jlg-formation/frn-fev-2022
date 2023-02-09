253b01bf4224c548d5dee0b8805ac84c1c33bfb1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-02-09 14:09:02 +0100

    dev: adding bottom tabs navigation
    with the icons 😊

--- front/screens/MenuScreen.tsx
+++ front/screens/MenuScreen.tsx
@@ -1,21 +1,43 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {HomeScreen} from './HomeScreen';
 import {LegalScreen} from './LegalScreen';
 import {RootStackParamList} from './navigation';
 import {SettingsScreen} from './SettingsScreen';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator<RootStackParamList>();
 
 function MenuScreen() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Legal" component={LegalScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Home"
+        screenOptions={({route}) => ({
+          // eslint-disable-next-line react/no-unstable-nested-components
+          tabBarIcon: ({focused, color, size}) => {
+            let iconName = 'ios-list';
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Legal" component={LegalScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
